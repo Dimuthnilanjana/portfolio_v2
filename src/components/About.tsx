dbc4942461581218c3bde27c799c5e20b939a31a
Dimuthnilanjana <dimuthnilanjana7@gmail.com>
2025-02-14 00:52:13 +0530

    dev_configurations

---
+++ src/components/About.tsx
@@ -0,0 +1,165 @@
+"use client";
+import { Paragraph } from "@/components/Paragraph";
+import Image from "next/image";
+import { AnimatedTooltip } from "../components/ui/animated-tooltip";
+import { motion } from "framer-motion";
+
+export default function About() {
+  const images = [
+    "https://images.unsplash.com/photo-1692544350322-ac70cfd63614?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=800&q=60",
+    "https://images.unsplash.com/photo-1692374227159-2d3592f274c9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw4fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=800&q=60",
+    "https://images.unsplash.com/photo-1692005561659-cdba32d1e4a1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
+    "https://images.unsplash.com/photo-1692445381633-7999ebc03730?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzM3x8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
+  ];
+  const people = [
+    {
+      id: 1,
+      name: "John Doe",
+      designation: "Software Engineer",
+      image:
+        "https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80",
+    },
+    {
+      id: 2,
+      name: "Robert Johnson",
+      designation: "Product Manager",
+      image:
+        "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60",
+    },
+    {
+      id: 3,
+      name: "Jane Smith",
+      designation: "Data Scientist",
+      image:
+        "https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60",
+    },
+    {
+      id: 4,
+      name: "Emily Davis",
+      designation: "UX Designer",
+      image:
+        "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGF2YXRhcnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+    },
+    {
+      id: 5,
+      name: "Tyler Durden",
+      designation: "Soap Developer",
+      image:
+        "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3540&q=80",
+    },
+    {
+      id: 6,
+      name: "Dora",
+      designation: "The Explorer",
+      image:
+        "https://images.unsplash.com/photo-1544725176-7c40e5a71c5e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3534&q=80",
+    },
+  ];
+  return (
+    <div>
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-10 my-10">
+        {images.map((image, index) => (
+          <motion.div
+            key={image}
+            initial={{
+              opacity: 0,
+              y: -50,
+              rotate: 0,
+            }}
+            animate={{
+              opacity: 1,
+              y: 0,
+              rotate: index % 2 === 0 ? 3 : -3,
+            }}
+            transition={{ duration: 0.2, delay: index * 0.1 }}
+          >
+            <Image
+              src={image}
+              width={200}
+              height={400}
+              alt="about"
+              className="rounded-md object-cover transform rotate-3 shadow-xl block w-full h-40 md:h-60 hover:rotate-0 transition duration-200"
+            />
+          </motion.div>
+        ))}
+        {/* 
+        // <Image
+        //   src="https://images.unsplash.com/photo-1692544350322-ac70cfd63614?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=800&q=60"
+        //   width={200}
+        //   height={400}
+        //   alt="about"
+        //   className="rounded-md object-cover transform rotate-3 shadow-xl block w-full h-40 md:h-60 hover:rotate-0 transition duration-200"
+        // />
+        // <Image
+        //   src="https://images.unsplash.com/photo-1692374227159-2d3592f274c9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw4fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=800&q=60"
+        //   width={200}
+        //   height={400}
+        //   alt="about"
+        //   className="rounded-md object-cover transform -rotate-3 shadow-xl block w-full h-40 md:h-60  hover:rotate-0 transition duration-200"
+        // />
+        // <Image
+        //   src="https://images.unsplash.com/photo-1692005561659-cdba32d1e4a1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
+        //   width={200}
+        //   height={400}
+        //   alt="about"
+        //   className="rounded-md object-cover transform rotate-3 shadow-xl block w-full h-40 md:h-60  hover:rotate-0 transition duration-200"
+        // />
+        // <Image
+        //   src="https://images.unsplash.com/photo-1692445381633-7999ebc03730?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzM3x8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
+        //   width={200}
+        //   height={400}
+        //   alt="about"
+        //   className="rounded-md object-cover transform -rotate-3 shadow-xl block w-full h-40 md:h-60  hover:rotate-0 transition duration-200"
+        // /> */}
+      </div>
+      <div className="flex flex-row items-center justify-center mb-10 w-full">
+      <AnimatedTooltip items={people} />
+    </div>
+      <div className="max-w-4xl">
+        <Paragraph className=" mt-4">
+          Hey there, I&apos;m John Doe - a passionate developer, avid writer,
+          and a connoisseur of awesome design. Welcome to my corner of the
+          digital world!
+        </Paragraph>
+        <Paragraph className=" mt-4">
+          Since the early days of my journey, I&apos;ve been captivated by the
+          art of crafting exceptional digital experiences. As a developer, I
+          thrive on turning lines of code into functional and elegant solutions.
+          My goal is to not just create software, but to build digital marvels
+          that seamlessly merge form and function.
+        </Paragraph>
+
+        <Paragraph className=" mt-4">
+          But my journey doesn&apos;t stop at coding. With a heart full of words
+          and a mind brimming with ideas, I&apos;ve ventured into the realm of
+          writing. From tech articles that unravel complex concepts to creative
+          tales that ignite the imagination, I weave words to inform, entertain,
+          and inspire.
+        </Paragraph>
+        <Paragraph className=" mt-4">
+          What sets me apart is my unwavering appreciation for design. I believe
+          that aesthetics and usability go hand in hand. My eye for awesome
+          design ensures that every project I undertake not only works
+          flawlessly under the hood but also looks stunning on the surface.
+        </Paragraph>
+        <Paragraph className=" mt-4">
+          Through this website, I aim to share my insights, experiences, and
+          creations with you. Whether you&apos;re a fellow developer seeking
+          solutions, a fellow writer in search of inspiration, or simply someone
+          who appreciates the finer aspects of design, there&apos;s something
+          here for you.
+        </Paragraph>
+        <Paragraph className=" mt-4">
+          Join me on this journey of bytes and narratives, logic and creativity,
+          code and prose. Together, we can explore the boundless possibilities
+          of technology and storytelling, all while reveling in the sheer beauty
+          of thoughtful design.
+        </Paragraph>
+        <Paragraph className=" mt-4">
+          Thank you for being here, and I can&apos;t wait to embark on this
+          adventure with you.
+        </Paragraph>
+      </div>
+    </div>
+  );
+}
